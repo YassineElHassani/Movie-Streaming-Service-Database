60766e5d037c84c7981aaae9052be2b9a93e3c5a
Yassine El hassani <yassine.ds.404@gmail.com>
2024-12-09 15:18:29 +0100

    Creation of the table UserInfo

--- dataBase-Setup.sql
+++ dataBase-Setup.sql
@@ -11,3 +11,14 @@ CREATE TABLE subscription(
     subscriptionType VARCHAR(50) CHECK (subscriptionType IN ('Basic', 'Premium')),
     monthlyFee DECIMAL(10,2)
 );
+
+-- Creation of the table UserInfo
+USE MovieStreaming;
+CREATE TABLE userInfo(
+	userID INT PRIMARY KEY AUTO_INCREMENT,
+    firstName VARCHAR(100),
+    lastName VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    registration DATE,
+    subscriptionID INT
+);
