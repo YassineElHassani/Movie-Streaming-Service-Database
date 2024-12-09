e4f1a97bb5571af05b19abdec2becd2ff4e41fa2
Yassine El hassani <yassine.ds.404@gmail.com>
2024-12-09 15:30:49 +0100

    Creation of the table Movie

--- dataBase-Setup.sql
+++ dataBase-Setup.sql
@@ -9,16 +9,27 @@ USE MovieStreaming;
 CREATE TABLE subscription(
 	subscriptionID INT PRIMARY KEY AUTO_INCREMENT,
     subscriptionType VARCHAR(50) CHECK (subscriptionType IN ('Basic', 'Premium')),
-    monthlyFee DECIMAL(10,2)
+    monthlyFee DECIMAL(10,2) NOT NULL
 );
 
 -- Creation of the table UserInfo
 USE MovieStreaming;
 CREATE TABLE userInfo(
 	userID INT PRIMARY KEY AUTO_INCREMENT,
-    firstName VARCHAR(100),
-    lastName VARCHAR(100),
-    email VARCHAR(100) UNIQUE,
-    registration DATE,
-    subscriptionID INT
+    firstName VARCHAR(100) NOT NULL,
+    lastName VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    registration DATE NOT NULL,
+    subscriptionID INT NOT NULL
+);
+
+-- Creation of the table Movie
+USE MovieStreaming;
+CREATE TABLE movie(
+	movieID INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    genre VARCHAR(100) NOT NULL,
+    releaseYear INT NOT NULL,
+    duration INT NOT NULL,
+    rating VARCHAR(10) NOT NULL
 );
