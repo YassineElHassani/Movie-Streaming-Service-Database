4675a2921f27d94aa8f50e9c1ea4b28b84e2d455
Yassine El hassani <yassine.ds.404@gmail.com>
2024-12-09 14:36:50 +0100

    Creation of the table subscription

--- dataBase-Setup.sql
+++ dataBase-Setup.sql
@@ -1,5 +1,13 @@
 -- Name: Yassine El hassani
 -- Email: jinrox00@gmail.com
 
--- Creation  of the database
+-- Creation of the database
 CREATE DATABASE movieStreaming;
+
+-- Creation of the table subscription
+USE MovieStreaming;
+CREATE TABLE subscription(
+	subscriptionID INT PRIMARY KEY AUTO_INCREMENT,
+    subscriptionType VARCHAR(50) CHECK (subscriptionType IN ('Basic', 'Premium')),
+    monthlyFee DECIMAL(10,2)
+);
